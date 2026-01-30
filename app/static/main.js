76e7cf9c9dd7afed1ee6a5135cc04e77123c147e
mandar-saiyan87 <mdman2257@gmail.com>
2026-01-30 23:48:14 +0530

    date format function added

--- app/static/main.js
+++ app/static/main.js
@@ -1,5 +1,31 @@
+function formatTimestamp(isoString) {
+    const date = new Date(isoString);
 
+    const day = date.getUTCDate();
+    const suffix =
+        (day >= 11 && day <= 13) ? "th" :
+            (day % 10 === 1) ? "st" :
+                (day % 10 === 2) ? "nd" :
+                    (day % 10 === 3) ? "rd" : "th";
 
+    const dayWithSuffix = `${day}${suffix}`;
+
+    const month = date.toLocaleString("en-US", {
+        month: "long",
+        timeZone: "UTC"
+    });
+
+    const year = date.getUTCFullYear();
+
+    const time = date.toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "2-digit",
+        hour12: true,
+        timeZone: "UTC"
+    });
+
+    return `${dayWithSuffix} ${month} ${year} - ${time} UTC`;
+}
 
 
 
@@ -33,11 +59,11 @@ async function fetchEvents() {
         const li = document.createElement('li')
 
         if (event.action === 'push') {
-            li.innerText = `${event.author} pushed to ${event.to_branch}`
+            li.innerText = `${event.author} pushed to ${event.to_branch} on ${formatTimestamp(event.timestamp)}`
         }
 
         if (event.action === "pull_request") {
-            li.innerText = `${event.author} submitted a pull request from ${event.from_branch} to ${event.to_branch}`;
+            li.innerText = `${event.author} submitted a pull request from ${event.from_branch} to ${event.to_branch} on ${formatTimestamp(event.timestamp)} `
         }
 
         get_ul.appendChild(li)
